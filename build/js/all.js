beb0b6b6fe8f9122f3bfd6fb9abab6528677caed
alekseiBoiko <mrboikoaleksei@gmail.com>
2021-09-29 17:50:25 +0300

    related products carousel and comparison table

--- build/js/all.js
+++ build/js/all.js
@@ -9,8 +9,10 @@ function changeHamburger() {
 $(function(){
     changeHamburger();
 })
-function drawSlider(listClass) {
-    $(listClass).slick({
+// SLICK SLIDER
+
+function drawProductCardSlider(listClass) {
+    $('.product-img-list').slick({
         
         slidesToShow: 3,
         infinite: true,
@@ -18,8 +20,8 @@ function drawSlider(listClass) {
         slidesToScroll: 1,
         slide: 'li',
         arrows: true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-double-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-double-right"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-prev_card"><i class="fas fa-angle-double-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-prev slick-next_card"><i class="fas fa-angle-double-right"></i></button>',
         responsive: [
           {
             breakpoint: 1200,
@@ -46,8 +48,46 @@ function drawSlider(listClass) {
     });
 };
 
+function drawRelatedProductSlider(listClass) {
+  $('.product-list_related').slick({
+      
+      slidesToShow: 4,
+      infinite: true,
+      dots: false,
+      slidesToScroll: 1,
+      slide: 'li',
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev slick-prev_related"><i class="fas fa-long-arrow-alt-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next slick-next_related"><i class="fas fa-long-arrow-alt-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+};
+
 $(function(){
-    drawSlider('.product-img-list');
+  drawProductCardSlider();
+  drawRelatedProductSlider();
 })
 
 
--- src/js/drawSlider.js
+++ src/js/drawSlider.js
@@ -1,5 +1,7 @@
-function drawSlider(listClass) {
-    $(listClass).slick({
+// SLICK SLIDER
+
+function drawProductCardSlider(listClass) {
+    $('.product-img-list').slick({
         
         slidesToShow: 3,
         infinite: true,
@@ -7,8 +9,8 @@ function drawSlider(listClass) {
         slidesToScroll: 1,
         slide: 'li',
         arrows: true,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-double-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-double-right"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev slick-prev_card"><i class="fas fa-angle-double-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-prev slick-next_card"><i class="fas fa-angle-double-right"></i></button>',
         responsive: [
           {
             breakpoint: 1200,
@@ -35,7 +37,45 @@ function drawSlider(listClass) {
     });
 };
 
+function drawRelatedProductSlider(listClass) {
+  $('.product-list_related').slick({
+      
+      slidesToShow: 4,
+      infinite: true,
+      dots: false,
+      slidesToScroll: 1,
+      slide: 'li',
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev slick-prev_related"><i class="fas fa-long-arrow-alt-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next slick-next_related"><i class="fas fa-long-arrow-alt-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+};
+
 $(function(){
-    drawSlider('.product-img-list');
+  drawProductCardSlider();
+  drawRelatedProductSlider();
 })
 
